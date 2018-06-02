ca9352f5f6fa73feaee8fb6cce91ce342efed8d4
Dinesh <dineshudt17@gmail.com>
2018-06-02 18:28:00 +0530

    Just file movements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:recruitx/Interview.dart';
+import 'package:recruitx/interview_tab.dart';
 
 void main() => runApp(new MyApp());
 
@@ -32,78 +32,6 @@ class MyApp extends StatelessWidget {
           ]),
         ),
       ),
-    );
-  }
-}
-
-class InterviewsTab extends StatefulWidget {
-  InterviewsTab({Key key, this.isMyInterview}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return new _InterviewTab(isMyInterview);
-  }
-
-  final bool isMyInterview;
-}
-
-class _InterviewTab extends State<InterviewsTab> {
-  final bool isMyInterview;
-  final interviews = <Interview>[
-    new Interview("Dinesh", "Dev", 3),
-    new Interview("Ramesh", "Dev", 3),
-    new Interview("Rajesh", "QA", 8),
-    new Interview("Suresh", "BA", 5),
-  ];
-
-  _InterviewTab(this.isMyInterview);
-
-  @override
-  Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemBuilder: (context, index) {
-        if (index.isOdd) {
-          return new Divider();
-        }
-        var i = index ~/ 2;
-        if (i < interviews.length) {
-          var interview = interviews[i];
-          return new ListTile(
-            title: new InterviewCard(interview),
-          );
-        }
-      },
-    );
-  }
-}
-
-class InterviewCard extends StatelessWidget {
-  final Interview interview;
-
-  InterviewCard(this.interview);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        new Row(
-          children: [
-            new Text(interview.candidateName),
-            new Expanded(
-              child: new Text(
-                interview.role,
-                textAlign: TextAlign.end,
-              ),
-            ),
-          ],
-        ),
-        new Row(children: [
-          new Text(
-            interview.experience.toString() + " Yrs",
-          ),
-        ])
-      ],
     );
   }
 }
